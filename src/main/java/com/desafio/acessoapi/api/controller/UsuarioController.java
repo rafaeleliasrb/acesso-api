7956af1b2ab03dcf579800c500610da0ee047ac5
infra <infra@MGNOTE0770.mshome.net>
2019-12-11 16:12:57 -0300

    teste token

--- src/main/java/com/desafio/acessoapi/api/controller/UsuarioController.java
+++ src/main/java/com/desafio/acessoapi/api/controller/UsuarioController.java
@@ -1,23 +1,40 @@
 package com.desafio.acessoapi.api.controller;
 
 import java.net.URI;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.desafio.acessoapi.api.security.SecurityConstants;
 import com.desafio.acessoapi.domain.model.Usuario;
 import com.desafio.acessoapi.domain.service.UsuarioService;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 @RestController
-@RequestMapping(value = "/usuarios")
+@RequestMapping(value = "/api/public")
 public class UsuarioController {
 
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
 	
 	@Autowired
 	public UsuarioController(UsuarioService usuarioService) {
@@ -32,4 +49,43 @@ public class UsuarioController {
 		return ResponseEntity.created(usuarioUri).body(usuario);
 	}
 	
+	@GetMapping
+    public String getMessage() {
+        return "Hello from public API controller";
+    }
+	
+	
+	//https://dev.to/keysh/spring-security-with-jwt-3j76
+	public void teste() {
+		Authentication authentication = attemptAuthentication();
+		User user = ((User) authentication.getPrincipal());
+
+        List<String> roles = user.getAuthorities()
+            .stream()
+            .map(GrantedAuthority::getAuthority)
+            .collect(Collectors.toList());
+
+        byte[] signingKey = SecurityConstants.JWT_SECRET.getBytes();
+
+        String token = Jwts.builder()
+            .signWith(Keys.hmacShaKeyFor(signingKey), SignatureAlgorithm.HS512)
+            .setHeaderParam("typ", SecurityConstants.TOKEN_TYPE)
+            .setIssuer(SecurityConstants.TOKEN_ISSUER)
+            .setAudience(SecurityConstants.TOKEN_AUDIENCE)
+            .setSubject(user.getUsername())
+            .setExpiration(new Date(System.currentTimeMillis() + 864000000))
+            .claim("rol", roles)
+            .compact();
+
+        System.out.println("token: " + token);
+	}
+	
+	public Authentication attemptAuthentication() {
+        String username = "username";
+        String password = "password";
+        UsernamePasswordAuthenticationToken authenticationToken = 
+        		new UsernamePasswordAuthenticationToken(username, password);
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
 }
