2dd4b1745a6ba60049b2180ee94be2a6a38e6a56
Rafael Elias <13rafael.elias@gmail.com>
2019-12-13 19:31:41 -0300

    versao final

--- src/main/java/com/desafio/acessoapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/desafio/acessoapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.desafio.acessoapi.api.exceptionhandler;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.desafio.acessoapi.domain.exception.EmailJaUtilizadoException;
@@ -15,38 +16,43 @@ import com.desafio.acessoapi.domain.exception.UsuarioOuSenhaInvalidoException;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EmailJaUtilizadoException.class)
-	public ResponseEntity<Object> tratarEntidadeNaoEncontradaException(EmailJaUtilizadoException e) {
-		Problema problema = Problema.builder()
-				.mensagem(e.getMessage())
-				.build();
+	public ResponseEntity<Object> tratarEntidadeNaoEncontradaException(
+			EmailJaUtilizadoException ex, WebRequest request) {
 		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+				HttpStatus.BAD_REQUEST, request);
 	}
 	
 	@ExceptionHandler(UsuarioOuSenhaInvalidoException.class)
-	public ResponseEntity<Object> tratarUsuarioOuSenhaInvalidoException(UsuarioOuSenhaInvalidoException e) {
-		Problema problema = Problema.builder()
-				.mensagem(e.getMessage())
-				.build();
+	public ResponseEntity<Object> tratarUsuarioOuSenhaInvalidoException(
+			UsuarioOuSenhaInvalidoException ex, WebRequest request) {
 		
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(problema);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+				HttpStatus.UNAUTHORIZED, request);
 	}
 	
 	@ExceptionHandler(NaoAutorizadoException.class)
-	public ResponseEntity<Object> tratarNaoAutorizadoException(NaoAutorizadoException e) {
-		Problema problema = Problema.builder()
-				.mensagem(e.getMessage())
-				.build();
+	public ResponseEntity<Object> tratarNaoAutorizadoException(
+			NaoAutorizadoException ex, WebRequest request) {
 		
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(problema);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
+				HttpStatus.UNAUTHORIZED, request);
 	}
 	
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<Object> tratarHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
-		Problema problema = Problema.builder()
-				.mensagem(e.getMessage())
-				.build();
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
 		
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
+		if (body == null) {
+			body = Problema.builder()
+					.mensagem(status.getReasonPhrase())
+					.build();
+		} else if (body instanceof String) {
+			body = Problema.builder()
+					.mensagem((String) body)
+					.build();
+		}
+		
+		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 }
